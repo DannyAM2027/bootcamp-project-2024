9677fea1855bea65c87d8273e56933f14cadab68
Daniel Miranda <dmiran12@calpoly.edu>
2024-12-24 02:09:11 -0800

    fixed for deploying1275

--- bootcamp-milestone-2/src/app/page.tsx
+++ bootcamp-milestone-2/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 //import Navbar from "@/components/navbar";
 import styles from "./page.module.css";
 
-import Footer from '@/components/footer';
+//import Footer from '@/components/footer';
 
 export default function Home() {
   return (
